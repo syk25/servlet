5e44cf4eebb99b05849c06d7c956043c6ae63464
syk25 <syk2245@gmail.com>
2024-01-10 15:43:51 +0900

    feat: 서블릿으로 회원관리 프로그램 구현

--- src/test/java/hello/servlet/domain/member/MemberFormServlet.java
+++
@@ -1,8 +0,0 @@
-package hello.servlet.domain.member;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-@WebServlet(name="memberFormServlet", urlPatterns = "/servlet/members/new-form")
-public class MemberFormServlet extends HttpServlet {
-
-}
