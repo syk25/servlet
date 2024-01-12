769435e6fa898f3aa0609e80eb3a93b1854e307e
syk25 <syk2245@gmail.com>
2024-01-12 14:13:06 +0900

    refactor: spring MVC 활용을 위한 HandlerMapping, Handler Adapter를 사용하는 스프링 빈 추가

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,16 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") // 컨트롤러가 스프링 빈으로 등록될 때의 이름 - url 매핑이 되어 있음
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
